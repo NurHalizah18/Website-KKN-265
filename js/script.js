8d75c96b38c60a266e346fd8b1f3bbdc5840fe94
NurHalizah18 <nurhalizah181002@gmail.com>
2024-09-10 14:14:48 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// bagian navbar
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    menu.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".team-slider",{
+    loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        789: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
